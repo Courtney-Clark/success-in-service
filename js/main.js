3e9ba992628edb9ebaae1a74302f5830724fa780
Courtney-Clark <cclark2@peacecorps.gov>
2016-01-09 17:53:19 -0500

    added functions to change video panel

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 
  $(document).ready(function() {
 
-$(".change").click(function(){
-$(".featurevid").slideToggle("fast")
-$(".secondvid").show()
-})
+ $(".secondvid").hide();
+ $(".thirdvid").hide();
+
+function changeVid(){
+$(".featurevid").hide();
+$(".secondvid").show('slide', 400)
+}
+$(".change").click(changeVid)
+
+function changeVidTwo(){
+$(".secondvid").hide();
+$(".thirdvid").show('slide', 400)
+}
+$(".twochange").click(changeVidTwo)
+
+function changeVidThree(){
+$(".thirdvid").hide();
+$(".featurevid").show('slide', 400)
+}
+$(".threechange").click(changeVidThree)
+
 
 })
