aef445216b82b3fb80e73c6f98e683abce60969d
Courtney-Clark <cclark2@peacecorps.gov>
2016-01-18 20:06:58 -0500

    Created slideDown function with IDs

--- js/main.js
+++ js/main.js
@@ -1,52 +1,75 @@
 
- $(document).ready(function() {
+// $(document).ready(function() {
 
-$( '.dropdown' ).hover(
-        function(){
-            $(this).children('.childnav').slideDown(200);
-        },
-        function(){
-            $(this).children('.childnav').slideUp(200);
-        }
- );
-
-
- $(".secondvid").hide();
- $(".thirdvid").hide();
-
-function changeVid(){
-$(".featurevid").hide();
-$(".secondvid").show('slide', 400)
-}
-$(".change").click(changeVid)
-
-function changeVidTwo(){
-$(".secondvid").hide();
-$(".thirdvid").show('slide', 400)
-}
-$(".twochange").click(changeVidTwo)
-
-function changeVidThree(){
-$(".thirdvid").hide();
-$(".featurevid").show('slide', 400)
-}
-$(".threechange").click(changeVidThree)
-
-})
-
- //watch videos
-
- $(".readmore").click(function(){
-	event.preventDefault()
-    $("#show-this-on-click").slideDown();
-    $(".readless").show();
-    $(".readmore").hide();
-});
+// $( '.dropdown' ).hover(
+//         function(){
+//             $(this).children('.childnav').slideDown(200);
+//         },
+//         function(){
+//             $(this).children('.childnav').slideUp(200);
+//         }
+//  );
 
 
-$(".readless").click(function(){
-	event.preventDefault()
-	$("#show-this-on-click").slideUp();
-	$(".readless").hide();
-	$(".readmore").show();
-});
+//  $(".secondvid").hide();
+//  $(".thirdvid").hide();
+
+// function changeVid(){
+// $(".featurevid").hide();
+// $(".secondvid").show('slide', 400)
+// }
+// $(".change").click(changeVid)
+
+// function changeVidTwo(){
+// $(".secondvid").hide();
+// $(".thirdvid").show('slide', 400)
+// }
+// $(".twochange").click(changeVidTwo)
+
+// function changeVidThree(){
+// $(".thirdvid").hide();
+// $(".featurevid").show('slide', 400)
+// }
+// $(".threechange").click(changeVidThree)
+
+
+//  //watch videos
+
+//  function readMore () {
+// 	event.preventDefault()
+//     $(this).parent().next().slideDown(300);
+//     $(".readless").show();
+//     $(".readmore").hide();
+//     console.log ('test')
+//   }
+
+//  $(".readmore").click(readMore)
+
+//  function readLess () {
+//  	event.preventDefault()
+//   	$(this).parent().next().slideDown(300);
+// 	$(".readless").hide();
+// 	$(".readmore").show();
+//  }
+
+// $(".readless").click(readLess)
+
+// })
+
+$(document).ready(function() {
+   $("#readmore").click(function() {
+   		event.preventDefault()
+     	$("#showthisonclick").slideDown(300);
+     	$("#readmore").hide()
+     	$("#readless").show()
+      	console.log("test")
+    }); // end click
+
+   $("#readless").click(function(){
+   		event.preventDefault()
+   		$("#showthisonclick").slideUp(300);
+   		$("#readmore").show()
+   		$("#readless").hide()
+   })
+
+}); 
